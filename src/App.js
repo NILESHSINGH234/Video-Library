b2391ced19db9b5dabac53858467dbd4fe647125
Nilesh Kumar <131458541+NILESHSINGH234@users.noreply.github.com>
2024-03-17 22:24:01 +0530

    feat: added authentication

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import "./App.css";
 import logo from "./logo.png";
 import { Home } from "./pages/Home/Home";
 import { Trending } from "./pages";
+import { Login } from "./pages";
+import { Signup } from "./pages";
+import { useAuth } from "./context/AuthContext";
+import { Trending } from "./pages";
 
 function App() {
+  const {
+    state: { isLoggedIn },
+  } = useAuth();
   return (
     <>
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
-      <Route path="/trending" element={<Trending></Trending>}></Route>
+      {!isLoggedIn && <Route path="/signup" element={<Signup />} />}
+        {!isLoggedIn && <Route path="/login" element={<Login />} />}
+     <Route  path="/trending" element={<Trending></Trending>}></Route>
     </Routes>
     </>
   );
