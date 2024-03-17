823945caba1a2e55758aadf026abbe748ef7988e
Nilesh Kumar <131458541+NILESHSINGH234@users.noreply.github.com>
2024-03-17 21:42:20 +0530

    feat: added video listing page

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-
+import {BrowserRouter} from 'react-router-dom'
+import { VideoProvider } from "./context/VideoContext";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <VideoProvider>
     <App />
+    </VideoProvider>
+   
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
